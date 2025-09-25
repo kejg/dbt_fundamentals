97e5d4fe3b5885982d43b66175dbb0ca2226b4d7
kejg <94001264+kejg@users.noreply.github.com>
2025-09-25 09:17:53 +0200

    new changes for yml sources and stg + added tests

---
+++ models/test.sql
@@ -0,0 +1,16 @@
+with all_values as (
+
+    select
+        status as value_field,
+        count(*) as n_records
+
+    from analytics.dbt_ekarels.stg_jaffle_shop_orders
+    group by status
+
+)
+
+select *
+from all_values
+where value_field not in (
+    'placed','shipped','completed','returned'
+)
